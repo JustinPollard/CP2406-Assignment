3ffaad378adc7f0d8fa4b7cc550b4a655cbaf3ea
JustinPollard <30639894+JustinPollard@users.noreply.github.com>
2017-10-21 00:04:21 +1000

    Adding an array to store usernames.

--- src/Client.java
+++ src/Client.java
@@ -22,11 +22,13 @@ public class Client {
 
             //check if username is in the userList variable
             //assign userList variable with username and return a greeting to the user
-            if (!resultStr.equals(userList)) {
-                String userName = "Hello" + resultStr;
-                DatagramPacket user = new DatagramPacket(userName.getBytes(), userName.length(), address, 49152);
-                socket.send(user);
-                userList = resultStr;
+            if (userList.contains(resultStr)) {
+                //add resultStr to array
+                break;
+//                String userName = "Hello" + resultStr;
+//                DatagramPacket user = new DatagramPacket(userName.getBytes(), userName.length(), address, 49152);
+//                socket.send(user);
+//                userList = resultStr;
             }
             //if username is taken then send an 'incorrect' statement
             else {
