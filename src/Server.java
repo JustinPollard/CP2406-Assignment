d3acdd2ec3282b480092d9c128a15a8121262b78
iApplepet <justin.pollard@my.jcu.edu.au>
2017-10-25 14:41:46 +1000

    Fixing UDP in progress

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,63 @@
 import java.io.*;
 import java.net.*;
 
-public class Server{
+public class Server {
     static ServerSocket serverSocket;
     static Socket socket;
     static DataOutputStream out;
+    static Users[] user = new Users[20];
+    static DataInputStream in;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Server...");
         serverSocket = new ServerSocket(7777);
         System.out.println("Server started.");
-        socket = serverSocket.accept();
-        out = new DataOutputStream(socket.getOutputStream());
-        out.writeUTF("This is a test of Java sockets.");
+        while (true) {
+            socket = serverSocket.accept();
+            for (int i = 3; i < 20; i++) {
+                out = new DataOutputStream(socket.getOutputStream());
+                in = new DataInputStream(socket.getInputStream());
+                if (user[i] == null)
+                {
+                    user[i] = new Users(out, in, user);
+                    Thread thread = new Thread(user[i]);
+                    thread.start();
+                    break;
+                }
+                //out.writeUTF("This is a test of Java sockets.");
+                //System.out.println("Data has been sent.");
+            }
+        }
+    }
+
+    static class Users implements Runnable {
+
+        DataOutputStream out;
+        DataInputStream in;
+        Users[] user = new Users[20];
+
+        public Users(DataOutputStream out, DataInputStream in, Users[] user) {
+            this.out = out;
+            this.in = in;
+            this.user = user;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String message = in.readUTF();
+                    for (int i = 3; i < 20; i++) {
+                        if (user[i] != null) {
+                            user[i].out.writeUTF(message);
+                        }
+                    }
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
 
---
+++ src/ServerOld.java
@@ -0,0 +1,48 @@
+/*
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.ArrayList;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        InetAddress address = InetAddress.getByName("228.5.6.7");
+        MulticastSocket socket = new MulticastSocket(49152);
+
+        socket.joinGroup(address);
+        System.out.println("Connected. Waiting for player...");
+        ArrayList <String> userList = new ArrayList<>();
+
+        while (true) {
+            byte[] messageBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(messageBuffer, messageBuffer.length);
+
+            socket.receive(receivePacket);
+            String resultStr = new String(messageBuffer).trim();
+            System.out.println("received: " + resultStr);
+
+            //check if username is in the userList variable
+            //assign userList variable with username and return a greeting to the user
+            if (userList.contains(resultStr)) {
+                //add resultStr to array
+                String usedUserName = "Sorry, this name is taken.";
+                DatagramPacket usedName = new DatagramPacket(usedUserName.getBytes(),usedUserName.length(), address, 49152);
+                socket.send(usedName);
+//                String userName = "Hello" + resultStr;
+//                socket.send(user);
+//                userList = resultStr;
+            }
+            //if username is taken then send an 'incorrect' statement
+            else {
+
+                DatagramPacket user = new DatagramPacket(resultStr.getBytes(), resultStr.length(), address, 49152);
+                socket.send(user);
+                break;
+            }
+        }
+//following two code snippets don't work
+        socket.leaveGroup(address);
+        socket.close();
+    }
+}
+*/
